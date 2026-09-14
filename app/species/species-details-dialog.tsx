67ce6da86ae6d396908c6e9c0670296e4d3e4a7c
Antonchik2007 <antonkurbatov131@gmail.com>
2026-09-13 21:31:42 -0400

    Set up for part 2 implemented. Implementation of editing mechanism next

--- app/species/species-details-dialog.tsx
+++ app/species/species-details-dialog.tsx
@@ -1,11 +1,12 @@
 import type { Database } from "@/lib/schema";
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogTrigger, DialogContent, DialogTitle, DialogDescription, DialogHeader} from "@/components/ui/dialog";
+import { useState } from "react";
 
 
 type Species = Database["public"]["Tables"]["species"]["Row"];
 
-export default function SpeciesDetailsDialog({species}: {species: Species}){
+export default function SpeciesDetailsDialog({species, userId}: {species: Species; userId: string}){
 
     
     const descriptionProperties = {
@@ -13,9 +14,17 @@ export default function SpeciesDetailsDialog({species}: {species: Species}){
         "Total population": species.total_population,
         "Kingdom": species.kingdom
     };
-
+    const [editMode, setEditMode] = useState(false);
     return(
-    <Dialog>
+
+    editMode ? 
+    
+    ("Edit Functionality") 
+    
+    
+    : 
+        (
+        <Dialog>
         <DialogTrigger>
             <Button className="mt-3 w-full">Learn More</Button>
         </DialogTrigger>
@@ -23,14 +32,17 @@ export default function SpeciesDetailsDialog({species}: {species: Species}){
             <DialogHeader>
             <DialogTitle>{species.scientific_name}</DialogTitle>
                 {Object.entries(descriptionProperties).map(([label, value]) => (
-                    <p key={label}>{label}: {value}</p>
+                    value && <p key={label}>{label}: {value}</p>
                 ))}  
             <DialogDescription>
                 <p>Description: {species.description}</p>
             </DialogDescription>
             </DialogHeader>
+            {(userId == species.author) && <Button onClick={() => setEditMode(true)}>Edit The Specie</Button>}
         </DialogContent>
     </Dialog>
     )
+    )
+    
 }
 //scientific_name, common_name, total_population, kingdom, and description
